93784ce0d5aeb37cd855bc3e0f674f67e95ee879
stingel1 <stingel1@me>
2017-10-02 17:00:16 +0200

    este mala drobnost #5  Please enter the commit message for your changes. Lines starting

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("presun kamen 1 z %d na %d\n", a, b);
+    printf("presun kamen 1 z %d na %d\n.", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a);
-    printf("presun kamen %d z %d na %d\n", n, a, b);
+    printf("presun kamen %d z %d na %d\n.", n, a, b);
     hanoi(n, 6 - a - b, b);
   }
 }
